9e3b5e70554c53c4c246be4230c4ae8c91722ba4
xhaclicker <973926269@qq.com>
2017-11-10 02:17:31 +0800

    for ion change one

--- app/src/main/java/com/yaoyao/testall/activity/ForActivity.java
+++ app/src/main/java/com/yaoyao/testall/activity/ForActivity.java
@@ -4,8 +4,11 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 import com.yaoyao.testall.R;
 import com.yaoyao.testall.base.BaseActivity;
 
@@ -16,7 +19,8 @@ import com.yaoyao.testall.base.BaseActivity;
 public class ForActivity extends BaseActivity{
 
     TextView tv;
-    int showone;
+    int showone,forone;
+    EditText for_ed;
     @Override
     public void inidata() {
         
@@ -31,6 +35,7 @@ public class ForActivity extends BaseActivity{
     @Override
     public void iniview() {
         tv = (TextView) findViewById(R.id.for_text);
+        for_ed = (EditText) findViewById(R.id.for_ed);
         findViewById(R.id.for_bu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,8 +65,16 @@ public class ForActivity extends BaseActivity{
             super.handleMessage(msg);
             switch (msg.what){
                 case 1:
-                    if (showone<1000){
+                    forone = Integer.parseInt(for_ed.getText().toString());
+                    if (showone<forone){
                         showone++;
+                        Log.e(TAG, "handleMessage: "+showone );
+//                        new Thread(new Runnable() {
+//                            @Override
+//                            public void run() {
+////                                getIon();
+//                            }
+//                        }).start();
                         tv.setText(showone+"");
                         handler.sendEmptyMessageDelayed(1,1000);
                     }
@@ -69,5 +82,21 @@ public class ForActivity extends BaseActivity{
             }
         }
     };
+
+    private void getIon() {
+        Ion.with(this)
+                .load("http://203.223.146.182/Takeout/user/front/seebanner")
+                .setBodyParameter("loginid","54683154135321")
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        if (e!=null){
+                            Log.e(TAG, "onCompleted: 错误"+e.toString() );
+                        }
+
+                    }
+                });
+    }
 
 }
